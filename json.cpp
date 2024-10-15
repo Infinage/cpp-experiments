e0735c548b5b3ffb1884fcd19b46dde50d053f4d
infinage <37257700+Infinage@users.noreply.github.com>
2024-10-15 23:37:05 +0530

    Intermediate JSON play

--- json.cpp
+++ json.cpp
@@ -1,11 +1,35 @@
+#include <cctype>
+#include <cstddef>
 #include <iostream>
 #include <fstream>
 #include <stack>
 #include <string>
-#include <unordered_map>
+#include <variant>
 #include <vector>
+#include <map>
+
+using JSONSimple = std::variant<std::nullptr_t, std::string, int, double, bool>;
+using JSONCompound = std::variant<std::vector<JSONSimple>, std::map<JSONSimple, JSONSimple>>;
+
+class JSONParser {
+public:
+    static std::pair<int, std::string> extractString(std::size_t idx, std::size_t N, const std::string& json) {
+        std::string acc{""};
+        idx++;
+        while (idx < N && json[idx] != '"') {
+            acc += json[idx++];
+            if (json[idx] == '\\') {
+                acc += json[idx] + json[idx + 1];
+                idx += 2;
+            }
+        }
+        return {idx + 1, acc};
+    }
+};
 
 int main(int argc, char* argv[]) {
+    using JSONCombined = std::variant<std::string, JSONSimple, JSONCompound>;
+
     if (argc <= 1)
         std::cout << "Usage: ./json.out <filename>\n";
     else {
@@ -24,51 +48,36 @@ int main(int argc, char* argv[]) {
                 json += buffer + "\n";
             }
 
-            std::size_t N = json.size();
-
-            // Symbols
-            std::vector<std::string> tokens;
-            std::unordered_map<char, char> bracePairs({{'{', '}'}, {'[', ']'}});
-            std::stack<char> symStk;
-            std::size_t idx = 0;
-            while (idx < json.size()) {
-
-                // Process Strings - keys, values, list items
-                if (json[idx] == '"') {
-
-                    buffer.clear();
-                    idx++;
-                    while (idx < N && json[idx] != '"') {
-                        buffer += json[idx++];
-                        if (idx < N && buffer.back() == '\\')
-                            buffer += json[idx++];
+            std::size_t N = json.size(), i {0};
+            std::stack<JSONCombined> stk;
+            buffer.clear();
+            while (i < N){
+                if (json[i] == '{' || json[i] == '[')
+                    stk.push(std::string{json[i]});
+                else if (json[i] == '"') {
+                    std::tie(i, buffer) = JSONParser::extractString(i, N, json);
+                    stk.push(buffer);
+                } else if (json[i] == '}') {
+                    auto topElem = std::get_if<std::string>(&stk.top());
+                    std::map<JSONSimple, JSONSimple> mpp;
+                    while (topElem != nullptr && *topElem != "{") {
+                        JSONCombined key, value; 
+                        value = stk.top();
+                        stk.pop();
+                        key = stk.top();
+                        stk.pop();
+                        mpp[key] = value;
                     }
-                    tokens.push_back(buffer);
-                    idx++;
-                    buffer.clear();
-
+                } else if (json[i] == ']') {
+                    auto topElem = std::get_if<std::string>(&stk.top());
+                    std::vector<JSONSimple> vec;
+                    while (topElem != nullptr && *topElem != "{") {
+                        JSONCombined curr = stk.top();
+                        vec.push_back(curr);
+                    }
                 }
-
-                else if (json[idx] == '{' || json[idx] == '[') {
-                    symStk.push(json[idx++]);
-                }
-
-                else if (json[idx] == '}' || json[idx] == ']') {
-                    if (symStk.empty() || bracePairs[symStk.top()] != json[idx]) {
-                        std::cout << "Malformed json\n";
-                        return 1;
-                    } else
-                        symStk.pop();
-                    idx++;
-                }
-
-                else {
-                    idx++;
-                }
+                i++;
             }
-
-            for (std::string& tok: tokens)
-                std::cout << "Token: " << tok << "\n";
 
         }
     }
