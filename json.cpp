0a7c86228ef8671a5c4ea8265eb0908a4d8765e1
infinage <37257700+Infinage@users.noreply.github.com>
2024-10-16 22:45:49 +0530

    Day 3 - Json Parser

--- json.cpp
+++ json.cpp
@@ -1,15 +1,102 @@
+/*
+ * https://stackoverflow.com/questions/19543326/datatypes-for-representing-json-in-c
+ */
+
 #include <cctype>
 #include <cstddef>
+#include <exception>
 #include <iostream>
 #include <fstream>
 #include <stack>
+#include <stdexcept>
 #include <string>
 #include <variant>
 #include <vector>
-#include <map>
+#include <unordered_map>>
 
-using JSONSimple = std::variant<std::nullptr_t, std::string, int, double, bool>;
-using JSONCompound = std::variant<std::vector<JSONSimple>, std::map<JSONSimple, JSONSimple>>;
+/*
+ * Data structure to store the nodes in a JSON Document
+ */
+
+// Types of JSON objects
+enum NODE_TYPE: short {value, array, object};
+
+// Types of JSON Simple values
+using JSON_VALUE_TYPE = std::variant<std::string, std::nullptr_t, int, double, bool>;
+
+class JSONNode {
+    private:
+        std::string key;
+        const NODE_TYPE type;
+
+    public:
+        JSONNode(const std::string &k, NODE_TYPE t):
+            key(k), type(t) {};
+
+        NODE_TYPE getType() {
+            return type;
+        }
+
+        std::string getKey() {
+            return key;
+        }
+};
+
+class JSONValueNode: JSONNode {
+    private:
+        JSON_VALUE_TYPE value;
+
+    public:
+        JSONValueNode(const std::string &k, const JSON_VALUE_TYPE &v):
+            JSONNode(k, NODE_TYPE::value), value(v) {};
+};
+
+class JSONArrayNode: JSONNode {
+    private:
+        std::vector<JSONNode> values;
+
+    public:
+        JSONArrayNode(const std::string &k): JSONNode(k, NODE_TYPE::array) {};
+
+        void push(JSONNode node) {
+            values.push_back(node);
+        }
+
+        JSONNode pop() {
+            JSONNode result = values.back();
+            values.pop_back();
+            return result;
+        }
+
+        const JSONNode &operator[] (std::size_t idx) {
+            if (idx < values.size())
+                return values[idx];
+        }
+};
+
+class JSONObjectNode: JSONNode {
+    private:
+        std::vector<JSONNode> values;
+
+    public:
+        JSONObjectNode(const std::string &k): JSONNode(k, NODE_TYPE::object) {};
+
+        bool exists(std::string &k) {
+            for (JSONNode &node: values) {
+                if (node.getKey() == k)
+                    return true;
+            }
+            return false;
+        }
+
+        const JSONNode &operator[] (std::string &k) {
+            for (JSONNode &node: values) {
+                if (node.getKey() == k)
+                    return node;
+            }
+        }
+};
+
 
 class JSONParser {
 public:
@@ -28,7 +115,7 @@ public:
 };
 
 int main(int argc, char* argv[]) {
-    using JSONCombined = std::variant<std::string, JSONSimple, JSONCompound>;
+    using JSONCombined = std::variant<JSONSimple, JSONCompound>;
 
     if (argc <= 1)
         std::cout << "Usage: ./json.out <filename>\n";
@@ -58,9 +145,10 @@ int main(int argc, char* argv[]) {
                     std::tie(i, buffer) = JSONParser::extractString(i, N, json);
                     stk.push(buffer);
                 } else if (json[i] == '}') {
-                    auto topElem = std::get_if<std::string>(&stk.top());
-                    std::map<JSONSimple, JSONSimple> mpp;
-                    while (topElem != nullptr && *topElem != "{") {
+                    JSONCombined topElem = stk.top();
+                    stk.pop();
+                    std::map<JSONSimple, JSONCompound> mpp;
+                    while (topElem != nullptr && topElem != "{") {
                         JSONCombined key, value; 
                         value = stk.top();
                         stk.pop();
@@ -68,8 +156,9 @@ int main(int argc, char* argv[]) {
                         stk.pop();
                         mpp[key] = value;
                     }
+                    stk.push(mpp);
                 } else if (json[i] == ']') {
-                    auto topElem = std::get_if<std::string>(&stk.top());
+                    auto topElem = std::get_if<JSONSimple>(&stk.top());
                     std::vector<JSONSimple> vec;
                     while (topElem != nullptr && *topElem != "{") {
                         JSONCombined curr = stk.top();
