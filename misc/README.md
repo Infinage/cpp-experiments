cb698d62e631f9e4a65563e274dd4e5f720caecc
infinage <37257700+Infinage@users.noreply.github.com>
2024-11-10 08:34:51 +0530

    Sudoku: Generation logic, Update README

--- misc/README.md
+++ misc/README.md
@@ -14,6 +14,29 @@ Run the program with the number of rows and columns as arguments:
 ./build/minesweeper.out <rows> <cols>
 ```
 
+## Sudoku
+
+A CLI Sudoku generator and solver. The program can either generate a new Sudoku puzzle or solve an existing one from a file.
+
+### Usage
+
+1. Generate a new Sudoku puzzle: Run without a filename argument to generate a new puzzle.
+2. Solve a Sudoku puzzle: Provide a filename containing the puzzle grid to solve it.
+
+```
+# Generate a new Sudoku puzzle and save it to `test.txt`
+./build/sudoku.out | tee test.txt
+
+# Solve the Sudoku puzzle stored in `test.txt`
+./build/sudoku.out test.txt
+```
+
+If a second argument (filename) is provided, the program reads the puzzle from that file, solves it if there is a unique solution, and prints the solution to the console. If the puzzle has multiple solutions or is invalid, an error message is shown.
+
+### Input File Format
+
+The file should contain digits `1-9` and `.` characters to represent blank spaces. All other characters are ignored.
+
 ## Build Instructions
 
 To build the project, use the provided `Makefile`. It will create both debug and release versions of the executables.
--- misc/sudoku.cpp
+++ misc/sudoku.cpp
@@ -2,18 +2,21 @@
 #include <array>
 #include <fstream>
 #include <iostream>
-#include <numeric>
 #include <random>
+#include <sstream>
 
 using BOARD_T = std::array<std::array<char, 9>, 9>;
-using CANDIDATES_T = std::array<bool, 9>;
 
 class Sudoku {
     private:
+        // Canvas to solve and generate sudokus
         BOARD_T board;
+
+        // For random shuffling
         std::mt19937 random_gen;
 
-        CANDIDATES_T getCandidates(std::size_t row, std::size_t col) {
+        // Given a sudoku cell, lists all viable candidates for that cell
+        std::vector<std::size_t> getCandidates(std::size_t row, std::size_t col) {
             // Vector of bools denoting if that num is a viable candidate
             std::array<bool, 9> candidates;
             std::fill(candidates.begin(), candidates.end(), true);
@@ -35,67 +38,135 @@ class Sudoku {
                     if (board[i][j] != '.')
                         candidates[(std::size_t)board[i][j] - '0' - 1] = false;
 
-            return candidates;
+            // Bool array to indices vector
+            std::vector<std::size_t> indices;
+            for (std::size_t idx{0}; idx < 9; idx++)
+                if (candidates[idx])
+                    indices.push_back(idx);
+
+            return indices;
         }
 
+        /*
+        Recursively attempt filling the sudoku board. Non static in nature, solves the 'board' canvas
+        Randomized shuffles the candidates order
+            - If randomized we stop once a solution is reached - board is NOT cleared
+            - If unrandomized post solving, board is comes to its initial state
+        */
         bool backtrack(std::size_t row, std::size_t col, bool randomized = false) {
-            if (row == 9)
+            if (row == 9) {
+                solutions.push_back(board);
                 return true;
-            else if (col == 9)
-                return backtrack(row + 1, 0);
-            else if (board[row][col] != '.')
-                return backtrack(row, col + 1);
-            else {
-                CANDIDATES_T candidates{getCandidates(row, col)};
-
-                std::vector<std::size_t> indices(9);
-                std::iota(indices.begin(), indices.end(), 0);
+            } else if (col == 9) {
+                return backtrack(row + 1, 0, randomized);
+            } else if (board[row][col] != '.') {
+                return backtrack(row, col + 1, randomized);
+            } else {
+                std::vector<std::size_t> candidates{getCandidates(row, col)};
 
                 if (randomized)
-                    std::shuffle(indices.begin(), indices.end(), random_gen);
+                    std::shuffle(candidates.begin(), candidates.end(), random_gen);
 
-                for (const std::size_t i: indices)
-                    if (candidates[i]) {
-                        board[row][col] = (char)((i + 1) + '0');
-                        if (backtrack(row, col + 1))
-                            return true;
-                        board[row][col] = '.';
-                    }
+                for (const std::size_t i: candidates) {
+                    board[row][col] = (char)((i + 1) + '0');
+
+                    // Stop early if randomized, otherwise it might recurse forever
+                    bool result {backtrack(row, col + 1, randomized)};
+                    if (result && randomized)
+                        return true;
+
+                    board[row][col] = '.';
+                }
+
                 return false;
             }
         }
 
-        static BOARD_T createEmptyBoard() {
+        // Helper to init an empty board with all '.'(s)
+        static constexpr BOARD_T createEmptyBoard() {
             BOARD_T board;
             std::for_each(board.begin(), board.end(), [](std::array<char, 9> &row) { row.fill('.'); });
             return board;
         }
 
+        // Helper to generate pairs of all cells, from (0,0) to (8,8)
+        static constexpr std::vector<std::pair<std::size_t, std::size_t>> cells() {
+            std::vector<std::pair<std::size_t, std::size_t>> filledCells;
+            for (std::size_t i {0}; i < 9; i++)
+                for (std::size_t j {0}; j < 9; j++)
+                    filledCells.push_back({i, j});
+            return filledCells;
+        }
+
     public:
+        std::vector<BOARD_T> solutions;
         Sudoku (BOARD_T &board): board(board) {
-            std::random_device rd;
-            random_gen = std::mt19937(rd());
+            random_gen = std::mt19937{std::random_device{}()};
         }
 
         Sudoku() {
-            std::random_device rd;
-            random_gen = std::mt19937(rd());
+            random_gen = std::mt19937{std::random_device{}()};
             board = createEmptyBoard();
         }
 
-        bool solve(bool randomized) { return backtrack(0, 0, randomized); }
-
-        void print() {
-            std::string horizontalSep(25, '-');
-            std::cout << horizontalSep << "\n";
-            for (std::size_t i {0}; i < 9; i++) {
-                std::cout << "| ";
-                for (std::size_t j {0}; j < 9; j++)
-                    std::cout << board[i][j] << (j % 3 == 2? " | ": " ");
-                std::cout << "\n" << (i % 3 == 2? horizontalSep + "\n": "");
-            }
+        void solve() {
+            backtrack(0, 0, false); 
+            if (solutions.size() == 0)
+                std::cout << "Error: Invalid Sudoku.\n";
+            else if (solutions.size() > 1)
+                std::cout << "Error: Multiple solutions exist.\n";
+            else
+                print(solutions[0]);
         }
 
+        void generate() {
+            // Get random solved grid
+            backtrack(0, 0, true); 
+
+            // We'd be randomly remove numbers while there exists only a single solution
+            std::vector<std::pair<std::size_t, std::size_t>> filledCells {cells()};
+
+            // Shuffle to introduce randomness
+            std::shuffle(filledCells.begin(), filledCells.end(), random_gen);
+
+            // Start removal
+            while (!filledCells.empty()) {
+                auto [row, col] = filledCells.back();
+                filledCells.pop_back();
+                std::pair<std::pair<std::size_t, std::size_t>, char> lastRemoved {{row, col}, board[row][col]};
+                board[row][col] = '.';
+
+                // Unrandomized solving since only then we would generate all solutions
+                solutions.clear();
+                backtrack(0, 0, false); 
+
+                // Reinsert last removed if uniqueness not preserved
+                if (solutions.size() > 1)
+                    board[lastRemoved.first.first][lastRemoved.first.second] = lastRemoved.second;
+            }
+
+            // Print the unsolved board to console
+            print(board);
+        }
+
+        // Pretty print sudoku board
+        static void print(BOARD_T board) {
+            std::ostringstream oss;
+            std::string horizontalSep(25, '-');
+            oss << horizontalSep << "\n";
+            for (std::size_t i {0}; i < 9; i++) {
+                oss << "| ";
+                for (std::size_t j {0}; j < 9; j++)
+                    oss << board[i][j] << (j % 3 == 2? " | ": " ");
+                oss << "\n" << (i % 3 == 2? horizontalSep + "\n": "");
+            }
+            std::cout << oss.str();
+        }
+
+        /*
+         * Read BOARD from filename provided -
+         * All characters besides digits and '.' are ignored
+         */
         static BOARD_T read(std::string fname) {
             std::ifstream ifs{fname};
             if (!ifs) {
@@ -103,10 +174,24 @@ class Sudoku {
                 std::exit(1);
             } else {
                 BOARD_T grid;
-                for (std::size_t i {0}; i < 9; i++)
-                    for (std::size_t j {0}; j < 9; j++)
-                        ifs >> grid[i][j];
-                return grid;
+                char ch;
+                std::size_t row {0}, col {0};
+                while (ifs >> ch) {
+                    // Push to invalid range (out of bounds)
+                    if ((std::isdigit(ch) || ch == '.') && row == 9) { col++; break; }
+
+                    else if (std::isdigit(ch) || ch == '.') { grid[row][col++] = ch; }
+                    
+                    // Post reaching end of each row, jump to first cell of next row
+                    if (col == 9) { col = 0; row++; }
+                }
+                if (row == 9 && col == 0)
+                    return grid;
+                else {
+                    std::cout << "Error: Invalid input.\n"; 
+                    std::exit(1);
+                }
+
             }
         }
 };
@@ -117,13 +202,11 @@ int main(int argc, char **argv) {
         std::cout << "Usage: ./sudoku <filename>\n";
     } else if (argc == 1) {
         Sudoku game;
-        game.solve(true);
-        game.print();
+        game.generate();
     } else {
         BOARD_T grid {Sudoku::read(argv[1])};
         Sudoku game{grid};
-        game.solve(false);
-        game.print();
+        game.solve();
     }
 
     return 0;
