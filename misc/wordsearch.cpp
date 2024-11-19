fa444374aeae1cfa3d2cba58b2842e6174c9d975
infinage <37257700+Infinage@users.noreply.github.com>
2024-11-19 07:41:41 +0530

    WS: Improve comments + Refactor

--- misc/wordsearch.cpp
+++ misc/wordsearch.cpp
@@ -1,21 +1,14 @@
 #include <algorithm>
 #include <array>
-#include <cassert>
-#include <cctype>
-#include <cmath>
-#include <cstdlib>
-#include <cstring>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <limits>
-#include <memory>
 #include <random>
-#include <sstream>
-#include <stack>
 #include <string>
 #include <unordered_set>
 #include <vector>
+#include <stack>
 
 class WordSearch {
     private:
@@ -23,14 +16,12 @@ class WordSearch {
         std::vector<std::vector<char>> grid;
         std::vector<std::string> words;
 
-        // Overlaps, Rand int, row, col, dirx, diry
-        using GENERATE_CANDIDATE = std::tuple<int, int, int, int, int, int>;
-
         // For random number generation
         std::mt19937 random_gen;
         std::uniform_int_distribution<int> randInt;
         std::uniform_int_distribution<char> randChar;
 
+        // Trie data structure for efficient grid searches when solving
         class Trie {
             public:
                 bool end; 
@@ -62,6 +53,7 @@ class WordSearch {
                     curr->minDist = 0;
                 }
 
+                // Erase word from trie - update minDist
                 static bool erase(std::unique_ptr<Trie> &root, const std::string &word) {
                     Trie *curr = root.get(); 
                     std::stack<std::pair<Trie*, char>> stk;
@@ -102,42 +94,64 @@ class WordSearch {
                 }
         };
 
+        // Using coordinates with unordered_set
         struct HashPair {
             inline std::size_t operator() (const std::pair<int, int> &p) const {
                 return (std::size_t) ((31 * p.first) + p.second);
             }
         };
 
+        // Candidates are potentials grid coordinates a word could get inserted into
+        struct Candidate {
+            int overlaps;
+            int rand;
+            int row; int col;
+            int dirX; int dirY;
+        };
+
+        // Ordering the potential candidates during grid generation
         struct OrderCandidate {
-            inline bool operator() (const GENERATE_CANDIDATE &c1, const GENERATE_CANDIDATE &c2) const {
-                return std::get<0>(c1) < std::get<0>(c2) || (std::get<0>(c1) == std::get<0>(c2) && std::get<1>(c1) > std::get<1>(c2));
+            inline bool operator() (const Candidate &c1, const Candidate &c2) const {
+                return c1.overlaps < c2.overlaps || (c1.overlaps == c2.overlaps && c1.rand > c2.rand);
             }
         };
 
+        // Traversing along 8 directions on the grid
         constexpr static std::array<std::pair<int, int>, 8> dirs {{
             {-1, -1}, {-1, 0}, {-1, 1},
             { 0, -1},          { 0, 1},
             { 1, -1}, { 1, 0}, { 1, 1}
         }};
 
+        // Helper to check if given coordinates are valid
         bool isValid(int i, int j) {
             return 0 <= i && 0 <= j && i < (int)grid.size() && j < (int)grid[0].size();
         }
 
+        // Max dist we can go along a direction given row and col
+        // This is helpful to prune unneccessary traversals when word size exceeds available space
         std::size_t maxDist(int x, int y, int dx, int dy) {
             int bx {dx == 0? std::numeric_limits<int>::max(): dx < 0? 0: (int)grid.size()};
             int by {dy == 0? std::numeric_limits<int>::max(): dy < 0? 0: (int)grid[0].size()};
             return (std::size_t) std::min(std::abs(x - bx), std::abs(y - by)) + 1;
         }
 
-        std::vector<GENERATE_CANDIDATE> generateCandidates(std::string &word) {
-            // Iterate through all cells, in all directions
-            std::vector<GENERATE_CANDIDATE> result;
+        // Helper to init empty board given rows and cols
+        void initBoard(std::size_t rows, std::size_t cols) {
+            this->grid = std::vector<std::vector<char>>(rows, std::vector<char>(cols, '*'));
+        }
+
+        // Given a word, produces a sorted (by OrderCandidate) list
+        // of positions it could be inserted into
+        std::vector<Candidate> generateCandidates(std::string &word) {
+            std::vector<Candidate> result;
             for (std::size_t i{0}; i < grid.size(); i++) {
                 for (std::size_t j{0}; j < grid[0].size(); j++) {
                     for (auto [dx, dy]: dirs) {
-                        std::size_t idx {0};
+                        // Overlaps is one criteria by which we pick candidates
+                        // How many chars overlap with this candidate?
                         int x {(int)i}, y {(int)j}, overlaps {0};
+                        std::size_t idx {0};
                         bool candidateValid {isValid(x, y) && word.size() < maxDist(x, y, dx, dy)};
                         while (idx < word.size() && isValid(x, y) && candidateValid) {
                             char gridCh {grid[(std::size_t)x][(std::size_t)y]}, ch {word[idx++]};
@@ -147,8 +161,9 @@ class WordSearch {
                             } else if (gridCh != '*') overlaps++;
                             x += dx; y += dy;
                         }
+                        // Random int is used as a tie breaker
                         if (candidateValid)
-                            result.push_back({overlaps, randInt(random_gen), i, j, dx, dy});
+                            result.push_back({overlaps, randInt(random_gen), (int)i, (int)j, dx, dy});
                     }
                 }
             }
@@ -158,20 +173,28 @@ class WordSearch {
 
         /*
          * Pick a random word from the available pool of words
-         * Generate candidates for all words
-         * Iterate through all available candidates and try inserting them to see if they provide a valid grid
-         * If all combinations exhausted backtrack
+         * Generate candidates for all words, ordered by criteria*
+         * Iterate through all candidates
+         *  - Insert candidate and recurse to check if we can reach the solution
+         *  - If successful, return
+         *  - Else remove inserted word and try the next canidate
+         * If all combinations are exhausted for word, backtrack
          */
-        bool backtrackGenerate(std::vector<std::string> &words, int &backtrackThresh) {
+        bool backtrackGenerate(std::vector<std::string> &words, int threshold) {
             if (words.empty()) return true;
             else {
                 std::string word{words.back()};
                 words.pop_back();
-                std::vector<GENERATE_CANDIDATE> candidates {generateCandidates(word)};
-                for (GENERATE_CANDIDATE &candidate: candidates) {
+
+                // Only allow maximum threshold number of candidates
+                // We can do better by maintaining a priority queue of size `threshold`
+                std::vector<Candidate> allCandidates {generateCandidates(word)}, candidates;
+                candidates = std::vector<Candidate>{allCandidates.begin(), allCandidates.begin() + std::min(threshold, (int) allCandidates.size())};
+
+                for (Candidate &candidate: candidates) {
                     // Extract fields
-                    int row {std::get<2>(candidate)}, col {std::get<3>(candidate)};
-                    int dx {std::get<4>(candidate)}, dy {std::get<5>(candidate)};
+                    int row {candidate.row}, col {candidate.col};
+                    int dx {candidate.dirX}, dy {candidate.dirY};
 
                     // Insert word into grid
                     std::unordered_set<int> overlaps;
@@ -182,20 +205,18 @@ class WordSearch {
                         grid[(std::size_t)x][(std::size_t)y] = word[(std::size_t)idx];
                     }
 
-                    if (backtrackGenerate(words, backtrackThresh))
+                    // Does this config produce a valid grid?
+                    if (backtrackGenerate(words, threshold))
                         return true;
 
-                    // Backtrack - remove inserted word
+                    // Remove inserted word
                     for (int idx {0}; idx < (int) word.size(); idx++) {
                         int x {row + (idx * dx)}, y {col + (idx * dy)};
                         if (overlaps.find(idx) == overlaps.end())
                             grid[(std::size_t)x][(std::size_t)y] = '*';
                     }
-
-                    // One less backtrack allowed
-                    if (--backtrackThresh <= 0)
-                        break;
                 }
+                // Insert word back and backtrack
                 words.push_back(word);
                 return false;
             }
@@ -205,28 +226,15 @@ class WordSearch {
         // Constructor for solver
         WordSearch(std::vector<std::vector<char>> &grid, std::vector<std::string> &words): grid(grid), words(words) {}
 
-        // Constructor for puzzle generator
+        // Constructor for puzzle generator - Defer Grid initialization to `generate` function
         WordSearch(std::vector<std::string> &words): words(words) {
-            // Find max length of words
-            std::size_t maxLength {0};
-            for (const std::string &word: words)
-                maxLength = std::max(maxLength, word.size());
-
-            // Compute grid dimensions
-            std::uniform_int_distribution<int> randDims{std::uniform_int_distribution<int>(
-                (int)(std::max(words.size() * .5, maxLength * 1.) * 1.45),
-                (int)(std::max(words.size() * .5, maxLength * 1.) * 1.85)
-            )};
-
             // For random shuffling during puzzle generation
             random_gen = std::mt19937{std::random_device{}()};
             randInt = std::uniform_int_distribution<int>(std::numeric_limits<int>::min(), std::numeric_limits<int>::max());
             randChar = std::uniform_int_distribution<char>('A', 'Z');
-
-            // Create empty grid with random dimensions
-            this->grid = std::vector<std::vector<char>>((std::size_t) randDims(random_gen), std::vector<char>((std::size_t) randDims(random_gen), '*'));
         }
 
+        // Solve puzzle using stack
         std::unordered_set<std::string> solve() {
             // Init a Trie object for efficient char matching
             std::unique_ptr<Trie> root {Trie::init(words)}; 
@@ -241,13 +249,15 @@ class WordSearch {
                         Trie* curr = root.get();
                         std::vector<std::pair<int, int>> path;
                         while (isValid(x, y) && curr->next[Trie::ord(grid[(std::size_t) x][(std::size_t) y])] != nullptr) {
-                            // Stop early by checking if the available words would fit in the direction
+                            // Stop early by checking if any of the available words would fit in the direction
                             if (curr->next[Trie::ord(grid[(std::size_t) x][(std::size_t) y])]->minDist > maxDist(x, y, dx, dy))
                                 break;
 
                             curr = curr->next[Trie::ord(grid[(std::size_t) x][(std::size_t) y])].get();
                             path.push_back({x, y}); 
                             x += dx; y += dy;
+
+                            // If end of a word is reached, inserted into solution but continue iterating
                             if (curr->end) {
                                 std::string acc{""};
                                 for (const std::pair<int, int> &p: path) {
@@ -255,6 +265,7 @@ class WordSearch {
                                     acc += grid[(std::size_t)p.first][(std::size_t)p.second];
                                 }
                                 found.insert(acc);
+                                Trie::erase(root, acc);
                             }
                         }
                     }
@@ -271,12 +282,36 @@ class WordSearch {
         }
 
         void generate() {
+            // Find avg length of words
+            std::size_t avgLength {0};
+            for (const std::string &word: words)
+                avgLength += word.size();
+            avgLength /= words.size();
+
+            // Compute grid dimensions
+            std::uniform_int_distribution<std::size_t> randDims{std::uniform_int_distribution<std::size_t>(
+                (std::size_t)(std::ceil(std::sqrt(words.size() * avgLength * (1 + 0.05)))),
+                (std::size_t)(std::ceil(std::sqrt(words.size() * avgLength * (1 + 0.25))))
+            )};
+
+            // Distribution to decide random increments
+            std::uniform_int_distribution<int> randChance(0, 1);
+
+            // Randomly pick starting dimensions
+            std::size_t rows {randDims(random_gen)}, cols {randDims(random_gen)};
             bool status;
+
             do {
-                // Restart after threshold num of steps
-                int cumulativeThresh {10};
+                // Create empty grid with random dimensions
+                initBoard(rows, cols);
+
+                // Increment rows / cols by chance on fail
+                if (randChance(random_gen)) rows++;
+                if (randChance(random_gen)) cols++;
+
+                // Shuffle and try to generate grid
                 std::shuffle(words.begin(), words.end(), random_gen);
-                status = backtrackGenerate(words, cumulativeThresh);
+                status = backtrackGenerate(words, 1);
             } while (!status);
 
             // Fill missing positions with random chars
@@ -286,6 +321,7 @@ class WordSearch {
                         grid[i][j] = randChar(random_gen);
         }
 
+        // Helper to pretty print the puzzle board
         void print() {
             std::ostringstream oss;
             for (std::size_t i{0}; i < grid.size(); i++) {
@@ -296,6 +332,7 @@ class WordSearch {
             std::cout << oss.str();
         }
 
+        // Helper to read a space / newline seperated list of words
         static std::vector<std::string> readWordList(std::string &&fname) {
             std::vector<std::string> result;
             if (std::filesystem::is_regular_file(fname)) {
@@ -307,6 +344,9 @@ class WordSearch {
             return result;
         }
 
+        // Helper to read the puzzle board
+        // Each row must be seperated by a new line
+        // Each char may be optionally space seperated (spaces are ignored)
         static std::vector<std::vector<char>> readGrid(std::string &&fname) {
             std::vector<std::vector<char>> result;
             if (std::filesystem::is_regular_file(fname)) {
