f63bb2a9d0ce2472dcb2af98722f1a1e68e65639
infinage <37257700+Infinage@users.noreply.github.com>
2024-12-10 22:54:24 +0530

    Design patterns: Singleton

---
+++ design-patterns/singleton.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <memory>
+#include <mutex>
+
+// A sample singleton class
+class Database {
+    private:
+        // Ensure that the lambda func is called only once
+        static std::once_flag flag;                                   
+
+        // Globally only one variable exists
+        static std::shared_ptr<Database> db;
+
+        // Mark constructor as private
+        Database() { std::cout << "Created a Database instance.\n"; }
+
+    public:
+        static std::shared_ptr<Database> getInstance() {
+            std::call_once(flag, []() { db = std::shared_ptr<Database>(new Database()); }); 
+            return db;
+        }
+};
+
+// Assign static variable values
+std::shared_ptr<Database> Database::db = nullptr;
+std::once_flag Database::flag;
+
+// Lets test things out
+int main() {
+    // Try creating multiple instances
+    std::shared_ptr<Database> conn1 {Database::getInstance()};
+    std::shared_ptr<Database> conn2 {Database::getInstance()};
+
+    // Print the memory address of both the instances
+    std::cout << "Connection Obj1 addr#: " << conn1.get() << "\n";
+    std::cout << "Connection Obj2 addr#: " << conn2.get() << "\n";
+
+    return 0;
+}
