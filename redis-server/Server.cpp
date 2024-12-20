882f71e2b758a5a08ad9fbc00d9386157c90068e
infinage <37257700+Infinage@users.noreply.github.com>
2024-12-20 22:55:06 +0530

    Redis Server: Async Sockets

--- redis-server/Server.cpp
+++ redis-server/Server.cpp
@@ -1,7 +1,5 @@
 #include <algorithm>
 #include <arpa/inet.h>
-#include <asm-generic/socket.h>
-#include <atomic>
 #include <cctype>
 #include <csignal>
 #include <cstdint>
@@ -12,45 +10,47 @@
 #include <string>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <utility>
+#include <fcntl.h>
+#include <poll.h>
+#include <vector>
 
 #include "include/Node.hpp"
 #include "include/Cache.hpp"
 
-std::atomic<bool> serverRunning {true};
+bool serverRunning {true};
 
-int temp_socket_id;
+using SOCKET_INFO_VALUE_TYPE = std::tuple<short, std::string, std::size_t, std::stack<std::tuple<char, std::size_t>>>;
+std::unordered_map<int, SOCKET_INFO_VALUE_TYPE> socketInfo;
 
-void handleInterupt(int signal_) {
+void closeSockets(int signal_) {
     serverRunning = false;
-    close(temp_socket_id);
-    std::exit(1);
+    for (const std::pair<const int, SOCKET_INFO_VALUE_TYPE> &p: socketInfo)
+        close(p.first);
 }
 
-// Recv request from client until complete
-std::string readRequest(int client_fd) {
-    // Variables to store & process request 
-    std::string request {};
-    std::size_t currPos {0};
-    bool reqData {true};
-    std::stack<std::tuple<char, std::size_t>> stk;
-
+// Recv request from client return true / false based on status
+bool readRequest(int client_fd, std::string &request, std::size_t &currPos, std::stack<std::tuple<char, std::size_t>> &stk) {
     // Dummy message to return on error
-    std::string errorMessage = "-Error receiving data\r\n";
+    const std::string errorMessage = "-Error receiving data\r\n";
 
-    // Recv until all of request is read
-    while (reqData || currPos < request.size()) {
-        char buffer[1024] = {0};
-        bool recvOk {!reqData || recv(client_fd, buffer, sizeof(buffer), 0) != -1};
-        if (!recvOk) break;
+    // Read one portion at a time
+    char buffer[1024] = {0};
+    if (recv(client_fd, buffer, sizeof(buffer), 0) == -1) {
+        request = errorMessage;
+        return true;
+    }
 
-        // Append received data into request string
-        request += buffer;
+    // Append received data into request string
+    request += buffer;
 
+    // Try to check if received data is 'complete', return false otherwise
+    while (currPos < request.size()) {
         // We check for req end usually by counting '\r\n' except
         // when we are parsing a bulk string, in which case we count chars
         if (stk.empty() || std::get<0>(stk.top()) != '$') {
             std::size_t tokEnd {request.find("\r\n", currPos)};
-            if (tokEnd == std::string::npos) { reqData = true; } 
+            if (tokEnd == std::string::npos) { return false; } 
             else {
                 if (request.at(currPos) == '$' || request.at(currPos) == '*') {
                     std::size_t aggLength {
@@ -60,7 +60,6 @@ std::string readRequest(int client_fd) {
                     stk.push({request.at(currPos), aggLength});
                 } else { stk.push({request.at(currPos), 0}); }
 
-                reqData = false;
                 currPos = tokEnd + 2;
             }
         } 
@@ -73,18 +72,32 @@ std::string readRequest(int client_fd) {
 
              // If complete, update currPos to include '\r\n'
              if (std::get<1>(stk.top()) == 0) currPos += 2;
-             else reqData = true;
+             else return false;
         }
 
         // Pop the top most if we can and add to prev guy
         while (!stk.empty() && std::get<1>(stk.top()) == 0) {
              stk.pop();
-             if (stk.empty()) return request;
+             if (stk.empty()) return true;
              else stk.top() = {'*', std::get<1>(stk.top()) - 1};
         }
     }
-    
-    return errorMessage;
+
+    return false;
+}
+
+// Send response to client return true / false based on status
+bool sendResponse(int client_fd, std::string &response, std::size_t &currPos) {
+    long sent {send(client_fd, response.c_str(), currPos, 0)};
+    if (sent == -1) {
+        std::cerr << "Sending response to client failed.\n";
+        response = ""; currPos = 0;
+        return true;
+    }
+
+    response = response.substr(static_cast<std::size_t>(sent));
+    currPos -= static_cast<std::size_t>(sent);
+    return currPos == 0;
 }
 
 std::string handleCommandPing(std::shared_ptr<Redis::AggregateRedisNode> &args) {
@@ -125,12 +138,9 @@ std::string handleCommandGet(std::shared_ptr<Redis::AggregateRedisNode> &args, R
     }
 }
 
-void handleClient(int client_fd, Redis::Cache &cache) {
-    // Read request from client
-    std::string serializedRequest {readRequest(client_fd)};
-    std::shared_ptr<Redis::RedisNode> reqNode {Redis::RedisNode::deserialize(serializedRequest)};
-
+std::string handleRequest(std::string &request, Redis::Cache &cache) {
     // Process the request
+    std::shared_ptr<Redis::RedisNode> reqNode {Redis::RedisNode::deserialize(request)};
     std::string command;
     std::shared_ptr<Redis::AggregateRedisNode> args;
     if (reqNode->getType() == Redis::NODE_TYPE::PLAIN) {
@@ -150,7 +160,9 @@ void handleClient(int client_fd, Redis::Cache &cache) {
     }
 
     // Convert to lower case
-    std::transform(command.begin(), command.end(), command.begin(), [](const char ch) { return std::tolower(ch); });
+    std::transform(command.begin(), command.end(), command.begin(), [](const char ch) { 
+        return std::tolower(ch); 
+    });
 
     // Prepare a suitable response
     std::string serializedResponse;
@@ -165,9 +177,15 @@ void handleClient(int client_fd, Redis::Cache &cache) {
     else
         serializedResponse = Redis::PlainRedisNode("Not supported", false).serialize();
 
-    // Send reponse to client
-    send(client_fd, serializedResponse.c_str(), serializedResponse.size(), 0);
-    close(client_fd);
+    // Return serialized response
+    return serializedResponse;
+}
+
+std::vector<pollfd> createPollInput() {
+    std::vector<pollfd> result;
+    for (const std::pair<const int, SOCKET_INFO_VALUE_TYPE> &p: socketInfo)
+        result.push_back({p.first, std::get<0>(p.second), 0});
+    return result;
 }
 
 int main() {
@@ -179,15 +197,18 @@ int main() {
     // Create an instance of Redis Cache
     Redis::Cache cache;
 
+    // Create sockaddr struct for port + IP binding
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    inet_pton(AF_INET, SERVER_IP, &serverAddr.sin_addr);
+    serverAddr.sin_port = htons(PORT);
+
     // Create server socket
-    int server_fd {socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)};
+    int server_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (server_fd == -1) {
         std::cerr << "Socket could not be initialized.\n";
         std::exit(1);
     }
-
-    // DEBUG
-    temp_socket_id = server_fd;
 
     // Set SO_REUSEADDR option
     int opt = 1;
@@ -196,11 +217,15 @@ int main() {
         std::exit(1);
     }
 
+    // Set server to nonblocking mode
+    int fcntl_flags {fcntl(server_fd, F_GETFL, 0)};
+    if(fcntl_flags == -1 || fcntl(server_fd, F_SETFL, fcntl_flags | O_NONBLOCK) == -1) {
+        std::cerr << "Socket could not be set to nonblocking mode.\n";
+        close(server_fd);
+        std::exit(1);
+    }
+
     // Bind socket to IP and Port
-    sockaddr_in serverAddr;
-    serverAddr.sin_family = AF_INET;
-    inet_pton(AF_INET, SERVER_IP, &serverAddr.sin_addr);
-    serverAddr.sin_port = htons(PORT);
     if (bind(server_fd, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(sockaddr_in)) == -1) {
         std::cerr << "Error binding socket to port.\n";
         std::exit(1);
@@ -212,24 +237,70 @@ int main() {
         std::exit(1);
     }
 
-    std::signal(SIGINT, handleInterupt);
+    // Add server_fd to list of sockets to monitor and maintain
+    socketInfo[server_fd] = {POLLIN, "", 0, std::stack<std::tuple<char, std::size_t>>{}};
+
+    std::signal(SIGINT, closeSockets);
     while (serverRunning) {
-        // Accept incomming connection
-        sockaddr_in clientAddr;
-        socklen_t addr_size {sizeof(clientAddr)};
-        int client_fd {accept(server_fd, reinterpret_cast<sockaddr*>(&clientAddr), &addr_size)};
+        std::vector<pollfd> pollInput {createPollInput()};
+        int pollResult {poll(pollInput.data(), pollInput.size(), -1)};
+        if (pollResult == -1) {
+            std::cerr << "Poll failed.\n";
+            closeSockets(0); break;
+        }
+        for (std::size_t i{0}; i < pollInput.size(); i++) {
+            // Accept incomming connection
+            if ((pollInput[i].revents & POLLIN) && (pollInput[i].fd == server_fd)) {
+                sockaddr_in clientAddr;
+                socklen_t addr_size {sizeof(clientAddr)};
+                int client_fd {accept(server_fd, reinterpret_cast<sockaddr*>(&clientAddr), &addr_size)};
+                if (serverRunning && client_fd != -1) {
+                    // Set client to nonblocking mode
+                    int fcntl_flags {fcntl(client_fd, F_GETFL, 0)};
+                    if(fcntl_flags == -1 || fcntl(client_fd, F_SETFL, fcntl_flags | O_NONBLOCK) == -1) {
+                        std::cerr << "Socket could not be set to nonblocking mode.\n";
+                        close(client_fd);
+                    }
 
-        // Parse the IP client address
-        in_addr ipAddr {clientAddr.sin_addr};
-        char clientIP[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &ipAddr, clientIP, INET_ADDRSTRLEN);
+                    // Add new client to socketinfo
+                    socketInfo[client_fd] = {POLLIN, "", 0, {}};
+                }
+            } 
 
-        if (serverRunning && client_fd != -1) {
-            handleClient(client_fd, cache);
+            // Recv from client and process it
+            else if ((pollInput[i].revents & POLLIN) && (pollInput[i].fd != server_fd)) {
+                // Extract variables from socketInfo
+                int client_fd {pollInput[i].fd};
+                std::string &request {std::get<1>(socketInfo[client_fd])};
+                std::size_t &currPos {std::get<2>(socketInfo[client_fd])};
+                std::stack<std::tuple<char, std::size_t>> &stk {std::get<3>(socketInfo[client_fd])};
+
+                // Read request from client
+                bool status {(readRequest(client_fd, request, currPos, stk))};
+                if (!status) {
+                    // Read pending data
+                    socketInfo[client_fd] = {POLLIN, request, currPos, stk};
+                } else {
+                    // Process the request and prepare to send data to client
+                    std::string serializedResponse {handleRequest(request, cache)};
+                    socketInfo[client_fd] = {POLLOUT, serializedResponse, serializedResponse.size(), {}};
+                }
+            }
+
+            // Sent to client and reset its state back to POLLIN
+            else if((pollInput[i].revents & POLLOUT) && (pollInput[i].fd != server_fd)) {
+                // Extract variables from socketInfo
+                int client_fd {pollInput[i].fd};
+                std::string &response {std::get<1>(socketInfo[client_fd])};
+                std::size_t &currPos {std::get<2>(socketInfo[client_fd])};
+
+                // Send pending data or reset
+                if (!sendResponse(client_fd, response, currPos)) socketInfo[client_fd] = {POLLOUT, response, currPos, {}};
+                else socketInfo[client_fd] = {POLLIN, "", 0, {}};
+            }
         }
     }
 
     close(server_fd);
-
     return 0;
 }
