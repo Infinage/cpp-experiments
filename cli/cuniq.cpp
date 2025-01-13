4b91a06af53d31a63f192f9bfa436ee4020f4118
infinage <37257700+Infinage@users.noreply.github.com>
2025-01-13 12:19:41 +0530

    Cuniq: Refactor

--- cli/cuniq.cpp
+++ cli/cuniq.cpp
@@ -6,20 +6,181 @@
 #include <functional>
 #include <iostream>
 #include <istream>
+#include <memory>
 #include <ostream>
+#include <queue>
 #include <sstream>
 #include <string>
 #include <system_error>
 
-const std::string CUNIQ_VERSION = "1.0.0";
-const std::string versionMessage {
+// Helper to parse an int from a string - throw err on fail
+void parseInt(std::string& charStr, int& result, const std::string &msgOnFail) {
+    std::from_chars_result validInt {std::from_chars(charStr.data(), charStr.data() + charStr.size(), result)};
+    if(validInt.ec != std::errc{} || validInt.ptr != charStr.data() + charStr.size()) {
+        std::cerr << msgOnFail << "\n";
+        std::exit(1);
+    }
+}
+
+class Uniq {
+    private:
+        // Config variables
+        mutable bool countFlag, repeatFlag, uniqueFlag;
+        mutable bool ignoreCase, allRepeated, zeroTerminated;
+        mutable int compareCharsCnt, skipCharsCnt, skipFieldsCnt;
+        mutable std::shared_ptr<std::istream> ip;
+        mutable std::shared_ptr<std::ostream> op;
+
+        // Helper to tokenize and split strings based on whitespace
+        std::istringstream tokenizer;
+        std::string tokenizeAndSkipWhitespace(const std::string &str){
+            tokenizer.clear();
+            tokenizer.str(str);
+            std::string line, token;
+            for (int i {0}; i < skipFieldsCnt && tokenizer >> line; i++);
+            std::getline(tokenizer, token, zeroTerminated? '\0': '\n');
+            return token;
+        };
+
+        // Compare two char - case sensitivity changes based on config
+        bool cmpChars(const char ch1, const char ch2) {
+            return ignoreCase? std::tolower(ch1) == std::tolower(ch2): ch1 == ch2; 
+        }
+
+        // Helper function to compare two string based (optionally ignore case)
+        bool stringMatch (const std::string &str1_, const std::string &str2_) {
+            // Skip fields if set
+            std::string str1 {tokenizeAndSkipWhitespace(str1_)}, str2 {tokenizeAndSkipWhitespace(str2_)};
+
+            // Proceed as usual
+            int str1Size {static_cast<int>(str1.size()) - skipCharsCnt}, str2Size {static_cast<int>(str2.size()) - skipCharsCnt};
+            str1Size = std::max(0, str1Size); str2Size = std::max(0, str2Size);
+            if (compareCharsCnt == -1 && str1Size != str2Size) 
+                return false;
+            else if (compareCharsCnt != -1 && (std::min(str1Size, compareCharsCnt) != std::min(str2Size, compareCharsCnt))) 
+                return false;
+            else {
+                int boundedCompareChars {compareCharsCnt == -1? str1Size: std::min(str1Size, compareCharsCnt)};
+                std::string::const_iterator beg1 {str1.cbegin() + skipCharsCnt}, beg2 {str2.cbegin() + skipCharsCnt};
+                return std::equal(beg1, beg1 + boundedCompareChars, beg2, [this](const char ch1, const char ch2) { return cmpChars(ch1, ch2); });
+            }
+        }
+
+        // Helper function to print based on condition
+        void printAndClear(std::queue<std::string> &lines) {
+            std::size_t linesCount {lines.size()};
+            if (allRepeated) {
+                if (linesCount > 1) {
+                    for (std::size_t i {0}; i < linesCount; i++) {
+                        std::string &line {lines.front()};
+                        *op << line << (zeroTerminated? '\0': '\n');
+                        lines.pop();
+                    }
+                }
+            } else {
+                if (countFlag && (!repeatFlag || linesCount > 1) && (!uniqueFlag || linesCount == 1))
+                    *op << std::string(6, ' ') << linesCount << ' ';
+
+                if ((!repeatFlag || linesCount > 1) && (!uniqueFlag || linesCount == 1)) {
+                    *op << lines.front() << (zeroTerminated? '\0': '\n');
+                }
+
+            }
+
+            // Clear the queue
+            lines = {};
+        }
+
+    public:
+        // Static constants
+        static const std::string CUNIQ_VERSION;
+        static const std::string VERSION_MESSAGE;
+        static const std::string HELP_MESSAGE;
+
+        // Default constructor
+        Uniq() {
+            (*this)
+                .setInputFile("-")
+                .setOutputFile("-")
+                .setCountFlag(false)
+                .setRepeatFlag(false)
+                .setUniqueFlag(false)
+                .setSkipCharsCount(0)
+                .setSkipFieldsCount(0)
+                .setAllRepeatedFlag(false)
+                .setIgnoreCaseFlag(false)
+                .setCompareCharsCount(-1)
+                .setZeroTerminatedFlag(false);
+        }
+
+        // Builder pattern setters - bool
+        Uniq          &setCountFlag(bool flag) { countFlag      = flag; return *this; }
+        Uniq         &setRepeatFlag(bool flag) { repeatFlag     = flag; return *this; }
+        Uniq         &setUniqueFlag(bool flag) { uniqueFlag     = flag; return *this; }
+        Uniq     &setIgnoreCaseFlag(bool flag) { ignoreCase     = flag; return *this; }
+        Uniq    &setAllRepeatedFlag(bool flag) { allRepeated    = flag; return *this; }
+        Uniq &setZeroTerminatedFlag(bool flag) { zeroTerminated = flag; return *this; }
+
+        // Builder pattern setters - int
+        Uniq    &setSkipCharsCount(int   cnt) { skipCharsCnt    = cnt; return *this; }
+        Uniq   &setSkipFieldsCount(int   cnt) { skipFieldsCnt   = cnt; return *this; }
+        Uniq &setCompareCharsCount(int   cnt) { compareCharsCnt = cnt; return *this; }
+        
+        // Builder pattern setters - I/O
+        Uniq &setInputFile(const std::string &fname) {
+            if (fname.empty() || fname == "-")
+                // No deletion for std::cin
+                ip = std::shared_ptr<std::istream>(&std::cin, [](std::istream*) {});
+            else {
+                ip = std::make_shared<std::ifstream>(fname); 
+                if (ip->fail()) {
+                    std::cerr << "cuniq: " << fname << ": No such file or directory\n";
+                    std::exit(1);
+                }
+            }
+
+
+            return *this;
+        }
+
+        Uniq &setOutputFile(const std::string &fname) {
+            if (fname.empty() || fname == "-")
+                op = std::shared_ptr<std::ostream>(&std::cout, [](std::ostream*) {});
+            else {
+                op = std::make_shared<std::ofstream>(fname); 
+                if (op->fail()) {
+                    std::cerr << "cuniq: " << fname << ": No such file or directory\n";
+                    std::exit(1);
+                }
+            }
+
+            return *this;
+        }
+
+        // Execute command and output
+        void execute() {
+            std::queue<std::string> prevLines;
+            std::string line;
+            while (std::getline(*ip, line, zeroTerminated? '\0': '\n')) {
+                if (!prevLines.empty() && !stringMatch(line, prevLines.front()))
+                    printAndClear(prevLines);
+                prevLines.push(line);
+            }
+
+            // Print the left over line
+            printAndClear(prevLines);
+        }
+};
+
+const std::string Uniq::CUNIQ_VERSION = "1.0.0";
+const std::string Uniq::VERSION_MESSAGE {
     "cuniq (CPP Experiments) " + CUNIQ_VERSION + "\n"
     "License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.\n"
     "This is free software: you are free to change and redistribute it.\n"
     "There is NO WARRANTY, to the extent permitted by law.\n\n"
     "Written by Naresh Jagadeesan.\n"
 };
-const std::string helpMessage {
+const std::string Uniq::HELP_MESSAGE {
     "Usage: cuniq [OPTION]... [INPUT [OUTPUT]]\n"
     "Filter adjacent matching lines from INPUT (or standard input),\n"
     "writing to OUTPUT (or standard output).\n\n"
@@ -42,184 +203,104 @@ const std::string helpMessage {
 
 int main(int argc, char **argv) {
 
+    // Config variables
     std::string ifname, ofname;
-    bool countFlag {false}, repeatFlag {false}; 
-    bool uniqueFlag {false}, ignoreCase {false};
-    bool allRepeated {false}, zeroTerminated {false};
+    bool countFlag {false}, repeatFlag {false}, uniqueFlag {false};
+    bool ignoreCase {false}, allRepeated {false}, zeroTerminated {false};
     int compareChars {-1}, skipChars {0}, skipFields {0};
+    int argIdx {1};
 
-    // Helper to parse an int from a string - throw err on fail
-    std::function<void(std::string&, int&)> parseInt {[](std::string &charStr, int &result) {
-        std::from_chars_result validInt {std::from_chars(charStr.data(), charStr.data() + charStr.size(), result)};
-        if(validInt.ec != std::errc{} || validInt.ptr != charStr.data() + charStr.size()) {
-            std::cerr << "cuniq: " << charStr << " invalid number of bytes to compare\n";
-            std::exit(1);
-        }
-    }};
+    std::function<void(std::string&, int&, int&, const std::string&, const std::string&)> processIntArg {
+        [&argc, &argv]
+            (std::string &arg, int &argIdx, int &result, const std::string &shortP, const std::string &longP) {
+                std::string charsStr;
+                if (arg == shortP || arg == longP) {
+                    if (argIdx + 1 < argc) charsStr = argv[++argIdx];
+                    else { std::cerr << "cuniq: option requires an argument -- '" << shortP.back() << "'\n"; std::exit(1); }
+                } else if (arg.size() >= longP.size() + 1 && arg.at(longP.size()) == '=') {
+                    charsStr = arg.substr(longP.size() + 1);
+                } else {
+                    std::cerr << "cuniq: unrecognized option '" << arg << "'\nTry 'cuniq --help for more information.\n'";
+                    std::exit(1);
+                }
 
-    int i {1};
-    while (i < argc) {
-        std::string arg {argv[i]};
-        if (arg == "-c" || arg == "--count") {
+                // Check if compareCharsStr is valid and store it into compareChars
+                parseInt(charsStr, result, "cuniq: " + charsStr + " invalid number of bytes to compare.");
+            }
+    };
+
+    while (argIdx < argc) {
+        std::string arg {argv[argIdx]};
+        if (arg == "-c" || arg == "--count")
             countFlag = true;
-        } else if (arg == "-d" || arg == "--repeated") {
+
+        else if (arg == "-d" || arg == "--repeated")
             repeatFlag = true;
-        } else if (arg == "-h" || arg.starts_with("--h")) {
-            std::cout << helpMessage;
+
+        else if (arg == "-h" || arg.starts_with("--h")) {
+            std::cout << Uniq::HELP_MESSAGE;
             std::exit(0);
-        } else if (arg == "-v" || arg == "--version") {
-            std::cout << versionMessage;
+        } 
+
+        else if (arg == "-v" || arg == "--version") {
+            std::cout << Uniq::VERSION_MESSAGE;
             std::exit(0);
-        } else if (arg == "-u" || arg == "--unique") {
+        } 
+
+        else if (arg == "-u" || arg == "--unique")
             uniqueFlag = true;
-        } else if (arg == "-z" || arg == "--zero-terminated") {
+
+        else if (arg == "-z" || arg == "--zero-terminated")
             zeroTerminated = true;
-        } else if (arg == "-i" || arg == "--ignore-case") {
+
+        else if (arg == "-i" || arg == "--ignore-case")
             ignoreCase = true;
-        } else if (arg == "-D" || arg == "--all-repeated") {
+
+        else if (arg == "-D" || arg == "--all-repeated")
             allRepeated = true;
-        } else if (arg == "-w" || arg.starts_with("--check-chars")) {
-            std::string compareCharsStr;
-            if (arg == "-w" || arg == "--check-chars") {
-                if (i + 1 < argc) compareCharsStr = argv[++i];
-                else { std::cerr << "cuniq: option requires an argument -- 'w'\n"; std::exit(1); }
-            } else if (arg.size() >= 14 && arg.at(13) == '=') {
-                compareCharsStr = arg.substr(14);
-            } else {
-                std::cerr << "cuniq: unrecognized option '" << arg << "'\nTry 'cuniq --help for more information.\n'";
-                std::exit(1);
-            }
 
-            // Check if compareCharsStr is valid and store it into compareChars
-            parseInt(compareCharsStr, compareChars);
+        else if (arg == "-w" || arg.starts_with("--check-chars"))
+            processIntArg(arg, argIdx, compareChars, "-w", "--check-chars");
 
-        } else if (arg == "-s" || arg.starts_with("--skip-chars")) {
-            std::string skipCharsStr;
-            if (arg == "-s" || arg == "--skip-chars") {
-                if (i + 1 < argc) skipCharsStr = argv[++i];
-                else { std::cerr << "cuniq: option requires an argument -- 's'\n"; std::exit(1); }
-            } else if (arg.size() >= 13 && arg.at(12) == '=') {
-                skipCharsStr = arg.substr(13);
-            } else {
-                std::cerr << "cuniq: unrecognized option '" << arg << "'\nTry 'cuniq --help for more information.\n'";
-                std::exit(1);
-            }
+        else if (arg == "-s" || arg.starts_with("--skip-chars"))
+            processIntArg(arg, argIdx, skipChars, "-s", "--skip-chars");
 
-            // Check if skipCharsStr is valid and store it into skipChars
-            parseInt(skipCharsStr, skipChars);
+        else if (arg == "-f" || arg.starts_with("--skip-fields"))
+            processIntArg(arg, argIdx, skipFields, "-f", "--skip-fields");
 
-        } else if (arg == "-f" || arg.starts_with("--skip-fields")) {
-            std::string skipFieldsStr;
-            if (arg == "-f" || arg == "--skip-fields") {
-                if (i + 1 < argc) skipFieldsStr = argv[++i];
-                else { std::cerr << "cuniq: option requires an argument -- 'f'\n"; std::exit(1); }
-            } else if (arg.size() >= 14 && arg.at(13) == '=') {
-                skipFieldsStr = arg.substr(14);
-            } else {
-                std::cerr << "cuniq: unrecognized option '" << arg << "'\nTry 'cuniq --help for more information.\n'";
-                std::exit(1);
-            }
-
-            // Check if skipFieldsStr is valid and store it into skipFields
-            parseInt(skipFieldsStr, skipFields);
-
-        } else if (arg.at(0) == '-' && arg.size() > 1) {
+        else if (arg.at(0) == '-' && arg.size() > 1) {
             std::cerr << "cuniq: unrecognized option '" << arg << "'\nTry 'cuniq --help for more information.\n'";
             std::exit(1);
         } 
-        else if (ifname.empty()) { ifname = arg; } 
-        else { ofname = arg; }
 
-        i++;
+        else if (ifname.empty()) ifname = arg;
+        else ofname = arg;
+
+        argIdx++;
     }
 
-    // repeatFlag & countFlag are incompatible
+    // Validate params - repeatFlag & countFlag are incompatible
     if (allRepeated && countFlag) {
         std::cerr << "cuniq: printing all duplicated lines and repeat counts is meaningless.\n"
-                  << "Try 'cuniq --help' for more information.";
+                  << "Try 'cuniq --help' for more information.\n";
         std::exit(1);
     }
 
-    // Create streams for buffering input / outputs
-    std::istream *inp {ifname.empty() || ifname == "-"? &std::cin: new std::ifstream{ifname}};
-    std::ostream *op {ofname.empty() || ofname == "-"? &std::cout: new std::ofstream{ofname}};
+    // Initalize the object
+    Uniq uniq;
+    uniq
+        .setInputFile(ifname)
+        .setOutputFile(ofname)
+        .setCountFlag(countFlag)
+        .setRepeatFlag(repeatFlag)
+        .setUniqueFlag(uniqueFlag)
+        .setSkipCharsCount(skipChars)
+        .setSkipFieldsCount(skipFields)
+        .setCompareCharsCount(compareChars)
+        .setIgnoreCaseFlag(ignoreCase)
+        .setAllRepeatedFlag(allRepeated)
+        .setZeroTerminatedFlag(zeroTerminated);
 
-    if (inp->fail()) {
-        std::cerr << "cuniq: test: No such file or directory\n";
-        std::exit(1);
-    }
-
-    std::function<std::string(const std::string&)> tokenizeAndSkipWhitespace {[&skipFields, &zeroTerminated](const std::string &str){
-        std::istringstream iss{str};
-        std::string line, token;
-        for (int i {0}; i < skipFields && iss >> line; i++);
-        std::getline(iss, token, zeroTerminated? '\0': '\n');
-        return token;
-    }};
-
-    // Helper function to compare two string based (optionally ignore case)
-    std::function<bool(const std::string&, const std::string&)> stringMatch {
-        [&skipChars, &compareChars, &ignoreCase, tokenizeAndSkipWhitespace]
-            (const std::string &str1_, const std::string &str2_) {
-                // Skip fields if set
-                std::string str1 {tokenizeAndSkipWhitespace(str1_)}, str2 {tokenizeAndSkipWhitespace(str2_)};
-
-                // Proceed as usual
-                int str1Size {static_cast<int>(str1.size()) - skipChars}, str2Size {static_cast<int>(str2.size()) - skipChars};
-                str1Size = std::max(0, str1Size); str2Size = std::max(0, str2Size);
-                if (compareChars == -1 && str1Size != str2Size) 
-                    return false;
-                else if (compareChars != -1 && (std::min(str1Size, compareChars) != std::min(str2Size, compareChars))) 
-                    return false;
-                else {
-                    int boundedCompareChars {compareChars == -1? str1Size: std::min(str1Size, compareChars)};
-                    std::string::const_iterator beg1 {str1.cbegin() + skipChars}, beg2 {str2.cbegin() + skipChars};
-                    return std::equal(beg1, beg1 + boundedCompareChars, beg2, [&ignoreCase](const char ch1, const char ch2) { 
-                        return ignoreCase? std::tolower(ch1) == std::tolower(ch2): ch1 == ch2; 
-                    });
-            }
-        }
-    };
-
-    // Helper function to print based on condition
-    std::function<void(std::string&, std::size_t)> print {
-        [op, &repeatFlag, &allRepeated, &uniqueFlag, &countFlag, &zeroTerminated] 
-            (std::string &line, std::size_t count) {
-                if (allRepeated) {
-                    if (count > 1) {
-                        for (std::size_t i {0}; i < count; i++)
-                            *op << line << (zeroTerminated? '\0': '\n');
-                    }
-                } else {
-                    if (countFlag && (!repeatFlag || count > 1) && (!uniqueFlag || count == 1))
-                        *op << std::string(6, ' ') << count << ' ';
-
-                    if ((!repeatFlag || count > 1) && (!uniqueFlag || count == 1))
-                        *op << line << (zeroTerminated? '\0': '\n');
-                }
-        }
-    };
-
-    // Main logic to print
-    std::size_t count {0};
-    std::string line, prevLine;
-    while (std::getline(*inp, line, zeroTerminated? '\0': '\n')) {
-        if (count == 0) {
-            prevLine = line;
-            count = 1;
-        } else if (!stringMatch(line, prevLine)) {
-            print(prevLine, count);
-            prevLine = line;
-            count = 1;
-        } else { 
-            count++; 
-        }
-    }
-
-    // Print the left over line
-    print(prevLine, count);
-
-    // Clean up
-    if (inp != &std::cin) delete inp;
-    if (op != &std::cout) delete op;
+    // Execute command
+    uniq.execute();
 }
