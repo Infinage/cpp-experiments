a6d128f39c74d944db547bb5158d600abcde4cc2
infinage <37257700+Infinage@users.noreply.github.com>
2025-02-26 21:39:47 +0530

    Threadpool: Stateful thread pools

--- misc/ThreadPool.hpp
+++ misc/ThreadPool.hpp
@@ -2,35 +2,44 @@
 
 #include <atomic>
 #include <condition_variable>
+#include <functional>
 #include <mutex>
 #include <queue>
 #include <thread>
+#include <variant>
 #include <vector>
 
-template <class F>
+template <class F, class State = std::monostate>
 class ThreadPool {
     private:
         const std::size_t N_WORKERS;
         std::vector<std::thread> workers;
+        std::vector<State> states;
         std::queue<F> tasks;
         std::mutex taskMutex;
         std::condition_variable cv;
-        std::atomic<bool> exitCondition = false;
+        std::atomic<bool> exitCondition {false};
 
     public:
         ~ThreadPool() { join(); }
-        ThreadPool(const std::size_t N_WORKERS): N_WORKERS(N_WORKERS) {
+        ThreadPool(const std::size_t N_WORKERS, const std::function<State()> &initState = {}): 
+            N_WORKERS(N_WORKERS)
+        {
             for (std::size_t i {0}; i < this->N_WORKERS; i++) {
-                workers.push_back(std::thread([this]{
+                if (initState) states.emplace_back(initState());
+                workers.emplace_back(std::thread([this, i]{
                     for (;;) {
                         std::unique_lock<std::mutex> lock(taskMutex); 
                         cv.wait(lock, [this]{ return exitCondition || !tasks.empty(); });
                         if (tasks.empty() && exitCondition) return;
                         else {
-                            F task{tasks.front()}; 
+                            F task{std::move(tasks.front())}; 
                             tasks.pop();
                             lock.unlock();
-                            task();
+                            if constexpr(std::is_same_v<State, std::monostate>)
+                                task();
+                            else
+                                task(states[i]);
                         }
                     }
                 }));
@@ -43,14 +52,16 @@ class ThreadPool {
                 exitCondition = true;
                 cv.notify_all();
                 for (std::thread &worker: workers)
-                    worker.join();
+                    if (worker.joinable()) 
+                        worker.join();
             }
         }
 
         void enqueue(F &&task) {
-            taskMutex.lock();
-            tasks.push(task);
-            taskMutex.unlock();
+            {
+                std::lock_guard lock(taskMutex);
+                tasks.push(std::move(task));
+            }
             cv.notify_one();
         }
 };
