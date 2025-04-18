629111742131126df40f63bd4f6559bfc179a126
infinage <37257700+Infinage@users.noreply.github.com>
2025-04-18 21:28:19 +0530

    Mandelbrot Set Explorer: Enhancements

--- misc/mandelbrot.cpp
+++ misc/mandelbrot.cpp
@@ -1,13 +1,13 @@
-// $ g++ mandelbrot.cpp -std=c++23 -o mandelbrot -lsfml-graphics -lsfml-window -lsfml-system
+// $ g++ mandelbrot.cpp -std=c++23 -o mandelbrot -lsfml-graphics -lsfml-window -lsfml-system -O2
 
 /*
  * TODO:
- * 1. Implement `bigfloat` to support infinite zooms
+ * 1. Prevent zoom beyond double's precision
+ * 2. Implement `bigfloat` to support infinite zooms
  */
 
 #include <algorithm>
 #include <chrono>
-#include <complex>
 #include <stdexcept>
 
 #include <SFML/Graphics.hpp>
@@ -15,14 +15,31 @@
 #include "../cli/argparse.hpp"
 #include "../misc/threadPool.hpp"
 
-using namespace std::complex_literals;
+/* 
+ * Determines how quickly the complex number `cre + cim*i` diverges under the Mandelbrot iteration.
+ * Returns the number of iterations before divergence (max capped at `maxIterations`)
+ *
+ * Mandelbrot Equation: f(z) = z^2 + C
+ * For complex number z = a + bi:
+ *   z^2   = a^2 - b^2 + 2ab*i
+ *   Real  = a^2 - b^2
+ *   Imag  = 2ab
+ *
+ * On each iteration, update `re` and `im` based on z^2 + C
+ */
+unsigned check(const double cre, const double cim, unsigned maxIterations = 100, double boundary = 2.0) {
+    unsigned iterations {0}; 
+    double re {cre}, im {cim}, re2 {cre * cre}, im2 {cim * cim}; 
+    while (re2 + im2 < boundary * boundary && ++iterations < maxIterations) {
+        // `im` first otherwise modified `re`
+        // would impact `im` calculation
+        im = 2 * re * im + cim;
+        re = re2 - im2 + cre;
 
-// Check whether a given complex no is part of the mandelbrot set
-// Returns percentage of many iterations were run before diverging
-unsigned check(const std::complex<double> &C, unsigned maxIterations = 100, double boundary = 2.0) {
-    std::complex<double> curr{C}; unsigned iterations {0};
-    while (std::abs(curr) < boundary && ++iterations < maxIterations)
-        curr = curr * curr + C;
+        // Recompute
+        re2 = re * re;
+        im2 = im * im;
+    }
     return iterations;
 }
 
@@ -66,7 +83,6 @@ bool isInKeys(T key, std::initializer_list<T> keys) {
 }
 
 int main(int argc, char **argv) {
-
     // Specify command line args
     argparse::ArgumentParser parser{"mandelbrot"};
     parser.description(
@@ -76,8 +92,8 @@ int main(int argc, char **argv) {
         "Press 'z' to reset zoom level, and 's' to save a screenshot to the current directory."
     );
     parser.addArgument("n_iters", argparse::ARGTYPE::NAMED).alias("n").defaultValue(100)
-        .help("Iterations to run for checking divergence. Must be between (0, 1000]");
-    parser.addArgument("refresh_rate", argparse::ARGTYPE::NAMED).alias("r").defaultValue(0.15)
+        .help("Iterations to run for checking divergence. Must be between (0, 5000]");
+    parser.addArgument("refresh_rate", argparse::ARGTYPE::NAMED).alias("r").defaultValue(0.05)
         .help("Image is refreshed every `refresh_rate` seconds. Must be in float, eg: 1.");
     parser.addArgument("n_workers", argparse::ARGTYPE::NAMED).alias("j").defaultValue(4)
         .help("No. of concurrent threads to use for rendering image.");
@@ -86,7 +102,7 @@ int main(int argc, char **argv) {
     parser.parseArgs(argc, argv);
     int nIters {parser.get<int>("n_iters")}, nThreads {parser.get<int>("n_workers")}; 
     double rRate {parser.get<double>("refresh_rate")};
-    if (nIters <= 0 || nIters > 1000) throw std::runtime_error("`n_iters` must be between (0, 1000]");
+    if (nIters <= 0 || nIters > 5000) throw std::runtime_error("`n_iters` must be between (0, 5000]");
     if (rRate < 0) throw std::runtime_error("`refresh_rate` cannot be less than 0.");
     if (nThreads < 0) throw std::runtime_error("`n_workers` cannot be less than 0.");
 
@@ -107,7 +123,10 @@ int main(int argc, char **argv) {
     bool isMouseDragged {false}; int oldMouseX {-1}, oldMouseY {-1};
 
     // Cache image & redraw only on demand
-    sf::Image image; bool redraw {true}; sf::Clock clk;
+    sf::Texture texture;
+    texture.create(800, 600);
+    sf::Sprite mandelSprite {texture};
+    sf::Clock clk; bool redraw {true}; 
     sf::Cursor normalCursor, handCursor;
     normalCursor.loadFromSystem(sf::Cursor::Arrow);
     handCursor.loadFromSystem(sf::Cursor::SizeAll);
@@ -125,6 +144,8 @@ int main(int argc, char **argv) {
                 } case sf::Event::Resized: {
                     float width_ {static_cast<float>(evnt.size.width)}, height_ {static_cast<float>(evnt.size.height)};
                     window.setView(sf::View{sf::FloatRect{0, 0, width_, height_}});
+                    texture.create(evnt.size.width, evnt.size.height);
+                    mandelSprite.setTexture(texture, true);
                     redraw = true;
                     break;
                 } case sf::Event::MouseWheelScrolled: {
@@ -230,10 +251,10 @@ int main(int argc, char **argv) {
                     else if (key == sf::Keyboard::S) {
                         // Take a screenshot and save to png file with timestamp
                         sf::Vector2u winSize {window.getSize()};
-                        sf::Texture texture;
-                        texture.create(winSize.x, winSize.y);
-                        texture.update(window);
-                        sf::Image screenshot {texture.copyToImage()};
+                        sf::Texture screenshotTexture;
+                        screenshotTexture.create(winSize.x, winSize.y);
+                        screenshotTexture.update(window);
+                        sf::Image screenshot {screenshotTexture.copyToImage()};
                         std::chrono::time_point now {std::chrono::system_clock::now()};
                         long ms {std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count()};
                         screenshot.saveToFile("screenshot-" + std::to_string(ms) + ".png");
@@ -256,47 +277,28 @@ int main(int argc, char **argv) {
         if (redraw) {
             // Do nothing if we haven't hit the theshold yet
             if (clk.getElapsedTime().asSeconds() < rRate) continue;
-            else clk.restart();
+            else clk.restart(); redraw = false;
 
             // Get the window height and width and compute step values
             sf::Vector2u winSize {window.getSize()};
             unsigned HEIGHT {winSize.y}, WIDTH {winSize.x};
             double STEP_IM {(MAX_IM - MIN_IM) / HEIGHT}, STEP_RE {(MAX_RE - MIN_RE) / WIDTH};
-            image.create(WIDTH, HEIGHT); redraw = false;
-            unsigned maxBatchSize {(HEIGHT * WIDTH) / static_cast<unsigned>(nThreads)};
-            std::vector<std::tuple<unsigned, unsigned, double, double>> batch;
+            sf::Image image; image.create(WIDTH, HEIGHT);
             for (unsigned row {0}; row < HEIGHT; row++) {
-                for (unsigned col {0}; col < WIDTH; col++) {
-                    double re {MIN_RE + col * STEP_RE}, im {MIN_IM + row * STEP_IM};
-                    batch.emplace_back(row, col, re, im);
-                    if (batch.size() == maxBatchSize) {
-                        pool.enqueue([MAX_ITER, batch = std::move(batch), &image]() {
-                            for (auto [row_, col_, re_, im_]: batch) {
-                                unsigned iters {check({re_, im_}, MAX_ITER)};
-                                image.setPixel(col_, row_, getRGBColor(iters, MAX_ITER));
-                            }
-                        });
-                        batch.clear();
-                    }
-                }
-            }
-
-            // Queue the remaining batch if any
-            if (!batch.empty()) {
-                pool.enqueue([MAX_ITER, batch, &image]() {
-                    for (auto [row_, col_, re_, im_]: batch) {
-                        unsigned iters {check({re_, im_}, MAX_ITER)};
-                        image.setPixel(col_, row_, getRGBColor(iters, MAX_ITER));
+                pool.enqueue([=, &image]() {
+                    for (unsigned col {0}; col < WIDTH; col++) {
+                        double re {MIN_RE + col * STEP_RE}, im {MIN_IM + row * STEP_IM};
+                        unsigned iters {check(re, im, MAX_ITER)};
+                        image.setPixel(col, row, getRGBColor(iters, MAX_ITER));
                     }
                 });
             }
 
             // Draw the image
             pool.wait();
+            texture.update(image);
             window.clear(sf::Color::Black);
-            sf::Texture texture;
-            texture.loadFromImage(image);
-            window.draw(sf::Sprite{texture});
+            window.draw(mandelSprite);
             window.display();
         }
     }
