8697fb8dc15ecba544fe2dc8fcec8f7ac155e91d
infinage <37257700+Infinage@users.noreply.github.com>
2025-05-08 19:42:16 +0530

    Queens linkedin - write sol img

--- puzzles/queens.cpp
+++ puzzles/queens.cpp
@@ -10,15 +10,17 @@
 #include <unordered_set>
 #include <vector>
 
-#include "opencv2/highgui.hpp"
+#include "opencv2/imgcodecs.hpp"
 #include "opencv2/imgproc.hpp"
 
 class QueensSolver {
     private:
-        const std::vector<std::vector<char>> grid; 
-        std::size_t nQueens;
+        cv::Mat image;
+        const std::vector<cv::Rect> contours;
+        const std::size_t nQueens;
+        const std::vector<std::vector<unsigned>> grid; 
         std::unordered_set<std::size_t> cols;
-        std::unordered_set<char> regions; 
+        std::unordered_set<unsigned> regions; 
         std::vector<std::vector<bool>> solution;
 
         // Returns true if neighbours of said cell has a queen placed
@@ -52,7 +54,20 @@ class QueensSolver {
             regions.insert(grid[row][col]);
         }
 
-        static std::vector<std::vector<char>> readGrid(const std::string &fname) {
+        static void drawX(const cv::Mat &img, const cv::Rect &bbox) {
+            int dx {bbox.width / 10}; 
+            int dy {bbox.height / 10};
+
+            cv::Point p1 {bbox.x + dx, bbox.y + dy};
+            cv::Point p2 {bbox.x + bbox.width - dx, bbox.y + bbox.height - dy};
+            cv::Point p3 {bbox.x + bbox.width - dx, bbox.y + dy};
+            cv::Point p4 {bbox.x + dx, bbox.y + bbox.height - dy};
+
+            cv::line(img, p1, p2, cv::Scalar(0, 0, 255), 2);
+            cv::line(img, p3, p4, cv::Scalar(0, 0, 255), 2);
+        }
+
+        static cv::Mat readImage(const std::string &fname) {
             cv::Mat img {cv::imread(fname)};
             if (img.empty()) throw std::runtime_error("cannot read image");
 
@@ -80,56 +95,78 @@ class QueensSolver {
 
             // 4. Crop the image around region of interest
             img = img(cv::boundingRect(biggest)).clone(); 
+            return img;
+        }
 
-            // 5. Detect contours
+        static std::vector<cv::Rect> getCells(const cv::Mat &image) {
+            // 1. Detect contours
             cv::Mat gray, thresh;
-            cv::cvtColor(img, gray, cv::COLOR_RGB2GRAY);
+            cv::cvtColor(image, gray, cv::COLOR_BGR2GRAY);
             cv::threshold(gray, thresh, 0, 255, cv::THRESH_BINARY | cv::THRESH_OTSU);
             std::vector<std::vector<cv::Point>> rawContours;
             cv::findContours(thresh, rawContours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
             if (rawContours.empty()) throw std::runtime_error("No cell contours found.");
 
-            // 6. Filter out contours with areas too large or too small
+            // 2. Filter out contours with areas too large or too small
             std::ranges::nth_element(
                 rawContours, rawContours.begin() + rawContours.size() / 2, std::ranges::less{}, 
                 [](const std::vector<cv::Point> &cont) { return cv::contourArea(cont); }
             );
             auto filterContFunc {[mA = cv::contourArea(rawContours[rawContours.size() / 2])]
                 (const std::vector<cv::Point> &cont) {
-                    return std::fabs(cv::contourArea(cont) - mA) < 0.25;
+                    return (std::fabs(cv::contourArea(cont) - mA) / mA) < 0.3;
                 }
             };
-            std::vector<std::vector<cv::Point>> contours {
-                rawContours | std::ranges::views::filter(filterContFunc) | std::ranges::to<std::vector>()
+            std::vector<cv::Rect> contours {
+                rawContours 
+                    | std::ranges::views::filter(filterContFunc) 
+                    | std::ranges::views::transform([](const std::vector<cv::Point> &cont) { return cv::boundingRect(cont); })
+                    | std::ranges::to<std::vector>()
             };
 
-            // 7. Sort the filtered contours and apply non max supression
-            std::vector<std::vector<cv::Point>> finalBoxes;
-            std::ranges::sort(contours, [](const std::vector<cv::Point> &p1, const std::vector<cv::Point> &p2) -> bool {
-                cv::Rect rp1 {cv::boundingRect(p1)}, rp2 {cv::boundingRect(p2)};
-                return rp1.y < rp2.y || (rp1.y == rp2.y && rp1.x < rp2.x);
+            // 3. Sort the contours
+            std::ranges::sort(contours, [](const cv::Rect &rp1, const cv::Rect &rp2) -> bool {
+                return std::abs(rp1.y - rp2.y) > (rp1.height / 2)? 
+                    rp1.y < rp2.y: // Different rows
+                    rp1.x < rp2.x; // Different cols
             });
-            for (const std::vector<cv::Point> &cont: contours) {
 
+            // Ensure that we have a perfect square of cells
+            unsigned long nQueens {static_cast<unsigned long>(std::sqrt(contours.size()))};
+            if (nQueens * nQueens != contours.size())
+                throw std::runtime_error("Invalid grid, detected cells is not a square no: " 
+                        + std::to_string(contours.size()));
+
+            return contours;
+        }
+
+        std::vector<std::vector<unsigned>> constructGrid() {
+            // Convert to grid of characters
+            unsigned region {0};
+            std::vector<std::vector<unsigned>> grid(nQueens, std::vector<unsigned>(nQueens, 0));
+            std::unordered_map<double, unsigned> colorMapping;
+            for (std::size_t i {0}; i < contours.size(); ++i) {
+                const cv::Rect &bbox {contours[i]};
+                cv::Point center {bbox.x + bbox.width / 2, bbox.y + bbox.height / 2};
+                cv::Vec3b bgr {image.at<cv::Vec3b>(center)};
+                double avgColor = 0.114 * bgr[0] + 0.587 * bgr[1] + 0.299 * bgr[2];
+                if (colorMapping.find(avgColor) == colorMapping.end())
+                    colorMapping[avgColor] = region++;
+                unsigned ch {colorMapping[avgColor]};
+                grid[i / nQueens][i % nQueens] = ch;
+                cv::rectangle(image, bbox, cv::Scalar(0, 255, 0), 2);
+                cv::putText(image, std::to_string(ch), bbox.tl() + cv::Point(2, 12), 
+                        cv::FONT_HERSHEY_SIMPLEX, 0.4, cv::Scalar(0, 0, 255), 1);
             }
-
-            // DEBUG
-            std::size_t index {1};
-            for (const std::vector<cv::Point> &contour: contours) {
-                cv::Rect bbox = cv::boundingRect(contour);
-                cv::rectangle(img, bbox, cv::Scalar(0, 255, 0), 2);
-                cv::putText(img, std::to_string(index++), bbox.tl() + cv::Point(2, 12),
-                            cv::FONT_HERSHEY_SIMPLEX, 0.4, cv::Scalar(0, 0, 255), 1);
-            }
-            cv::imshow("debug", img); cv::waitKey(0);
-
-            throw "NOPE";
+            return grid;
         }
 
     public:
         QueensSolver(const std::string &fname): 
-            grid(readGrid(fname)), nQueens(this->grid.size()), 
-            solution(nQueens, std::vector<bool>(nQueens, false)) 
+            image(readImage(fname)), 
+            contours(getCells(image)),
+            nQueens(static_cast<unsigned long>(std::sqrt(contours.size()))), 
+            grid(constructGrid())
         {
             // Check - 1
             if (grid.empty() || grid.size() != grid[0].size())
@@ -140,13 +177,16 @@ class QueensSolver {
                 cols.insert(i);
 
             // Obtain the list of regions unfilled
-            for (const std::vector<char> &row: grid)
-                for (const char& ch: row)
+            for (const std::vector<unsigned> &row: grid)
+                for (const unsigned& ch: row)
                     regions.insert(ch);
 
             // Check - 2
             if (regions.size() != nQueens) 
                 throw std::runtime_error("Regions must equal the grid dimensions");
+
+            // Placeholder to hold the text rep solution
+            solution = std::vector<std::vector<bool>>(nQueens, std::vector<bool>(nQueens, false));
         }
 
         bool solve() {
@@ -192,19 +232,28 @@ class QueensSolver {
             }
             return oss.str();
         }
+
+        // Write solution to image file
+        void save(const std::string &fname) const {
+            cv::Mat solImg {image.clone()};
+            for (std::size_t i {0}; i < contours.size(); ++i) {
+                std::size_t row {i / nQueens}, col {i % nQueens};
+                const cv::Rect &bbox {contours[i]};
+                if (solution[row][col]) drawX(solImg, bbox);
+            }
+            cv::imwrite(fname, solImg);
+        }
 };
 
 int main(int argc, char **argv) {
-    if (argc != 2) std::cerr << "Usage queens <image>\n";
-    else {
-        // Solve the grid
+    if (argc != 3) {
+        std::cerr << "Usage queens <input_image> <output_image>\n";
+        return 1;
+    } else {
         QueensSolver solver(argv[1]);
         if (!solver.solve())
             throw std::runtime_error("No solution exists");
-
-        // Print out the solution
-        std::cout << solver.getString();
-
+        solver.save(argv[2]);
         return 0;
     }
 }
