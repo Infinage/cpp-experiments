b1a0d047923519be5aeb8c7b9fcf070fa14af442
infinage <37257700+Infinage@users.noreply.github.com>
2025-10-01 16:33:54 +0530

    Networking helpers: HTTPRequest

--- networking/net.hpp
+++ networking/net.hpp
@@ -2,19 +2,21 @@
 #include <cstdint>
 #include <memory>
 #include <netinet/in.h>
+#include <sstream>
 #include <stdexcept>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <unordered_map>
 #include <utility>
 #include <vector>
 
 namespace net {
-    [[nodiscard]] inline std::string resolveHostname(std::string_view hostname) {
+    [[nodiscard]] inline std::string resolveHostname(std::string_view hostname, const char *service = nullptr) {
         addrinfo hints{}, *res {};
         hints.ai_family = AF_INET;
         hints.ai_socktype = SOCK_STREAM;
-        if (getaddrinfo(hostname.data(), nullptr, &hints, &res) != 0)
+        if (getaddrinfo(hostname.data(), service, &hints, &res) != 0)
             throw std::runtime_error("Failed to resolve hostname");
 
         // Ensure res is freed always by wrapping inside a smart pointer
@@ -65,7 +67,7 @@ namespace net {
             }
 
             // Default ctor
-            Socket(SocketArgs args = { .domain = AF_INET, .type = SOCK_STREAM, .protocol = IPPROTO_TCP, .reuseSockAddr = true }) 
+            Socket(SocketArgs args = { .domain = PF_INET, .type = SOCK_STREAM, .protocol = IPPROTO_TCP, .reuseSockAddr = true }) 
             {
                 _fd = socket(args.domain, args.type, args.protocol);
                 if (_fd == -1) throw std::runtime_error("Error creating socket object");
@@ -84,14 +86,21 @@ namespace net {
                 if (bindStatus == -1) throw std::runtime_error("Error binding to the socket");
             }
 
-            void listen(unsigned short connections = 1) {
-                int listenStatus {::listen(_fd, connections)};
+            void listen(unsigned short backlog = 5) {
+                int listenStatus {::listen(_fd, backlog)};
                 if (listenStatus == -1) throw std::runtime_error("Error listening on socket");
             }
 
-            [[nodiscard]] Socket accept() {
-                int clientSocket {::accept(_fd, nullptr, nullptr)};
+            [[nodiscard]] Socket accept(std::string &host, std::uint16_t &port) {
+                sockaddr_in hostAddr {}; socklen_t hostAddrLen{sizeof(hostAddr)};
+                int clientSocket {::accept(_fd, reinterpret_cast<sockaddr*>(&hostAddr), &hostAddrLen)};
                 if (clientSocket == -1) throw std::runtime_error("Failed to accept an incomming connection");
+
+                port = ntohs(hostAddr.sin_port);
+                char ipStr[INET_ADDRSTRLEN];
+                inet_ntop(AF_INET, &hostAddr.sin_addr, ipStr, sizeof(ipStr));
+                host = ipStr;
+
                 return Socket{clientSocket};
             }
 
@@ -109,7 +118,7 @@ namespace net {
                 }
             }
 
-            std::string recv() {
+            [[nodiscard]] std::string recv() {
                 std::string message;
                 char buffer[2048] {}; long recvBytes;
                 while ((recvBytes = ::recv(_fd, buffer, sizeof(buffer), 0)) > 0)
@@ -123,7 +132,7 @@ namespace net {
                 if (sentBytes <= 0) throw std::runtime_error("Failed to send");
             }
 
-            std::string recvFrom(std::string &host, std::uint16_t &port, std::size_t maxBytes = 2048) {
+            [[nodiscard]] std::string recvFrom(std::string &host, std::uint16_t &port, std::size_t maxBytes = 2048) {
                 sockaddr_in hostAddr {}; socklen_t hostAddrLen {sizeof(hostAddr)};
                 std::vector<char> buffer(maxBytes); 
                 long recvBytes {::recvfrom(_fd, buffer.data(), maxBytes, 0, reinterpret_cast<sockaddr*>(&hostAddr), &hostAddrLen)};
@@ -135,6 +144,37 @@ namespace net {
                 host = ipStr;
 
                 return {buffer.data(), static_cast<std::size_t>(recvBytes)};
+            }
+    };
+
+    class HttpRequest {
+        private:
+            std::unordered_map<std::string, std::string> headers {{"Content-Type", "application/json"}, {"Connection", "close"}};
+            std::unordered_map<std::string, std::string> urlParams;
+            std::string path, method, body;
+
+            static std::string getPathWithParams(const std::string &path, const std::unordered_map<std::string, std::string> &params) {
+                if (params.empty()) return path;
+                std::ostringstream oss; oss << path << '?';
+                for (const auto &kv: params) oss << kv.first << "=" << kv.second << '&';
+                std::string res {oss.str()}; res.pop_back();
+                return res;
+            }
+
+        public:
+            HttpRequest(const std::string &path, const std::string &method, const std::string &body = ""): 
+                path{path}, method{method}, body{body} {}
+
+            void setHeader(const std::string &key, const std::string &value) { headers[key] = value; }
+            void setParam(const std::string &key, const std::string &value) { urlParams[key] = value; }
+
+            std::string serialize() const {
+                std::ostringstream oss;
+                oss << method << ' ' << getPathWithParams(path, urlParams) << " HTTP/1.1\r\n";
+                for (const auto &kv: headers) oss << kv.first << ": " << kv.second << "\r\n";
+                if (!body.empty()) oss << "Content-Length: " << body.size() << "\r\n\r\n" << body;
+                else oss << "\r\n";
+                return oss.str();
             }
     };
 }
